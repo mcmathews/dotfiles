07e761c96cf236c856c7925651eea2469fc0f22e
Michael Mathews <mcmathe1@asu.edu>
2016-01-31 19:02:20 -0700

    Add support for curl

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # dotfiles
 
 ## Setup
-* Install zsh and wget
+* Install zsh and wget or curl
 * `wget https://raw.githubusercontent.com/mcmathews/dotfiles/master/setup.sh`
 * `./setup.sh`
 * `rm setup.sh`
--- setup.sh
+++ setup.sh
@@ -1,16 +1,32 @@
-#!/bin/bash
+#!/bin/sh
+
+if ! which zsh &> /dev/null; then
+	echo "zsh must be installed"
+	exit 1
+fi
+
+if which wget &> /dev/null; then
+	DOWNLOAD="wget"
+	DOWNLOAD_TO_STDOUT="wget -O -"
+elif which curl &> /dev/null; then
+	DOWNLOAD="curl -O"
+	DOWNLOAD_TO_STDOUT="curl"
+else
+	echo "Either wget or curl must be installed"
+	exit 2
+fi
 
 ZSH=$HOME/.oh-my-zsh
 
 mkdir setuptmp
 cd setuptmp
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$($DOWNLOAD_TO_STDOUT https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-wget https://raw.githubusercontent.com/mcmathews/dotfiles/master/.git-prompt.sh
-wget https://raw.githubusercontent.com/mcmathews/dotfiles/master/hotchkis.zsh-theme
-wget https://raw.githubusercontent.com/mcmathews/dotfiles/master/.zshrc
-wget https://raw.githubusercontent.com/mcmathews/dotfiles/master/.gitconfig
-wget https://raw.githubusercontent.com/mcmathews/dotfiles/master/.vimrc
+$DOWNLOAD https://raw.githubusercontent.com/mcmathews/dotfiles/master/.git-prompt.sh
+$DOWNLOAD https://raw.githubusercontent.com/mcmathews/dotfiles/master/hotchkis.zsh-theme
+$DOWNLOAD https://raw.githubusercontent.com/mcmathews/dotfiles/master/.zshrc
+$DOWNLOAD https://raw.githubusercontent.com/mcmathews/dotfiles/master/.gitconfig
+$DOWNLOAD https://raw.githubusercontent.com/mcmathews/dotfiles/master/.vimrc
 
 mkdir $ZSH/custom/themes
 cp "hotchkis.zsh-theme" "$ZSH/custom/themes/"
